a29b4d9b4e34173258d92f7da199efd90e484118
claudia93L <34545518+claudia93L@users.noreply.github.com>
2023-11-02 16:31:51 +0100

    esercizio 8

--- Sett02/G03/01 - Esercizio/assets/js/D3.js
+++ Sett02/G03/01 - Esercizio/assets/js/D3.js
@@ -215,6 +215,14 @@ if (crewMass > 1000) {
   Usa un for loop per cambiare il valore della proprietà "gender" di alcuni personaggi dal valore "n/a" a "robot" (Tip: puoi effettuare la riassegnazione del valore corrispondente o creare un nuovo array)
 */
 
+for (let p = 0; p < starWarsCharacters.length; p++) {
+  if (starWarsCharacters[p].gender === 'n/a') {
+    starWarsCharacters[p].gender = 'robot';
+  }
+}
+
+console.log(starWarsCharacters);
+
 /* --EXTRA-- ESERCIZIO 9
   Utilizzando gli elementi presenti nell'array "femaleCharacters" rimuovi dall'array "characters" le stringhe corrispondenti a personaggi con lo stesso nome.
   Una volta fatto crea un console.log per controllare la proprietà length di "characters" prima e dopo l'operazione.
