9d352c7f95772b5747970830f3bf3e9c03f6c7d4
claudia93L <34545518+claudia93L@users.noreply.github.com>
2023-11-08 14:12:29 +0100

    es 1

--- Sett03/G02/01 - Esercizio/assets/script.js
+++ Sett03/G02/01 - Esercizio/assets/script.js
@@ -1,90 +1,96 @@
+``;
+
 /* ESERCIZIO 1
        Scrivi una funzione per cambiare il titolo della pagina in qualcos'altro
     */
 
-       const changeTitle = function () {}
+const changeTitle = function (nuovoTitolo) {
+  document.querySelector('h1').innerText = nuovoTitolo;
+};
 
-       /* ESERCIZIO 2
+changeTitle('Questo è il nuovo titolo della pagina');
+
+/* ESERCIZIO 2
         Scrivi una funzione per aggiungere al titolo della pagina una classe "myHeading"
      */
- 
-       const addClassToTitle = function () {}
- 
-       /* ESERCIZIO 3
+
+const addClassToTitle = function () {};
+
+/* ESERCIZIO 3
         Scrivi una funzione che cambi il testo dei p figli di un div
        */
- 
-       const changePcontent = function () {}
- 
-       /* ESERCIZIO 4
+
+const changePcontent = function () {};
+
+/* ESERCIZIO 4
         Scrivi una funzione che cambi la proprietà href di ogni link (tranne quello nel footer) con il valore https://www.google.com
        */
- 
-       const changeUrls = function () {}
- 
-       /* ESERCIZIO 5
+
+const changeUrls = function () {};
+
+/* ESERCIZIO 5
         Scrivi una funzione che aggiunga un nuovo elemento lista alla seconda lista non ordinata
      */
- 
-       const addToTheSecond = function () {}
- 
-       /* ESERCIZIO 6
+
+const addToTheSecond = function () {};
+
+/* ESERCIZIO 6
         Scrivi una funzione che aggiunga un paragrafo al primo div
      */
- 
-       const addParagraph = function () {}
- 
-       /* ESERCIZIO 7
+
+const addParagraph = function () {};
+
+/* ESERCIZIO 7
         Scrivi una funzione che faccia scomparire la prima lista non ordinata
      */
- 
-       const hideFirstUl = function () {}
- 
-       /* ESERCIZIO 8 
+
+const hideFirstUl = function () {};
+
+/* ESERCIZIO 8 
         Scrivi una funzione che renda verde il background di ogni lista non ordinata
        */
- 
-       const paintItGreen = function () {}
- 
-       /* ESERCIZIO 9
+
+const paintItGreen = function () {};
+
+/* ESERCIZIO 9
         Scrivi una funzione che rimuova l'ultima lettera dall'h1 ogni volta che l'utente lo clicca
        */
- 
-       const makeItClickable = function () {}
- 
-       /* ESERCIZIO 10
+
+const makeItClickable = function () {};
+
+/* ESERCIZIO 10
         Crea una funzione che, al click sul footer, riveli l'URL del link interno come contenuto di un alert()
        */
- 
-       const revealFooterLink = function () {}
- 
-       /* ESERCIZIO 11
+
+const revealFooterLink = function () {};
+
+/* ESERCIZIO 11
         Crea una funzione che crei una tabella nell'elemento con id "tableArea". 
         La tabella avrà 5 elementi e questa struttura: immagine, nome prodotto, quantità, prezzo
      */
- 
-       const generateTable = function () {}
- 
-       /* ESERCIZIO 12
+
+const generateTable = function () {};
+
+/* ESERCIZIO 12
         Crea una funzione che aggiunga una riga alla tabella precedentemente creata e fornisca i dati necessari come parametri
      */
- 
-       const addRow = function () {}
- 
-       /* ESERCIZIO 14
+
+const addRow = function () {};
+
+/* ESERCIZIO 14
        Crea una funzione che nasconda le immagini della tabella quando eseguita
      */
- 
-       const hideAllImages = function () {}
- 
-       /* EXTRA ESERCIZIO 15
+
+const hideAllImages = function () {};
+
+/* EXTRA ESERCIZIO 15
        Crea una funzione che cambi il colore del h2 con id "changeMyColor" con un colore random ad ogni click ricevuto
      */
- 
-       const changeColorWithRandom = function () {}
- 
-       /* EXTRA ESERCIZIO 16
+
+const changeColorWithRandom = function () {};
+
+/* EXTRA ESERCIZIO 16
        Crea una funzione che elimini le vocali da ogni elemento testuale della pagina (puoi aiutarti con i nuovi metodi degli array di ES6)
      */
- 
-       const deleteVowels = function () {}
+
+const deleteVowels = function () {};
