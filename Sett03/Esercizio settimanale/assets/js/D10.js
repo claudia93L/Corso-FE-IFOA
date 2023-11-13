5f9a6381f46655557fd8a26918a206276abd918c
claudia93L <34545518+claudia93L@users.noreply.github.com>
2023-11-13 15:05:24 +0100

    es 14

--- Sett03/Esercizio settimanale/assets/js/D10.js
+++ Sett03/Esercizio settimanale/assets/js/D10.js
@@ -455,6 +455,10 @@ console.log(countMovies2());
   Scrivi una funzione chiamata "onlyTheYears" che crea un array con solamente gli anni di uscita dei film contenuti nell'array "movies" fornito.
 */
 
+const onlyTheYears = () => movies.map((el) => el.Year);
+
+console.log(onlyTheYears());
+
 /* ESERCIZIO 15
   Scrivi una funzione chiamata "onlyInLastMillennium" che ritorna solamente i film prodotto nel millennio scorso contenuti nell'array "movies" fornito.
 */
