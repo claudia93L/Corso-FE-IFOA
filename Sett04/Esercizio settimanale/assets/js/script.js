ee7f59edc4ffebb0af934dd2942979b05a0a8ac3
claudia93L <34545518+claudia93L@users.noreply.github.com>
2023-11-30 09:26:16 +0100

    inizio lavoro sull'header

--- Sett04/Esercizio settimanale/assets/js/script.js
+++ Sett04/Esercizio settimanale/assets/js/script.js
@@ -1,12 +1,26 @@
-let header = document.getElementById('site-header-inner');
+let header = document.getElementById('header');
+let body = document.querySelector('body');
 
 document.addEventListener('scroll', function () {
-  // Get the scroll position
   let scrollPos = window.scrollY;
 
-  if (scrollPos > 100) {
-    header.style.backgroundColor = 'var(--primary-color)';
-  } else {
+  if (header.scrollTop > 350 || body.scrollTop > 350 ) {
     header.style.backgroundColor = '#FFF';
+    header.classList.remove('bg-yellow');
+  } else {
+    header.classList.remove('bg-scroll');
+    header.classList.add('bg-yellow');
   }
 });
+
+
+
+/* window.onscroll = function cambioColoreBackground () {
+    if (document.body.scrollTop > 350 || document.documentElement.scrollTop > 350) {
+        document.getElementById('buttonHeader').classList.add('green');
+        document.getElementById('header').classList.add('active');
+    } else  {
+        document.getElementById('buttonHeader').classList.remove('green');
+        document.getElementById('header').classList.remove('active');
+    }
+} */
