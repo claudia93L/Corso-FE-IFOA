d6b158ff0fab289de5c19c9fab5b1e1462d3239a
claudia93L <34545518+claudia93L@users.noreply.github.com>
2023-12-01 08:45:37 +0100

    header scroll completato

--- Sett04/Esercizio settimanale/assets/js/script.js
+++ Sett04/Esercizio settimanale/assets/js/script.js
@@ -1,26 +1,20 @@
 let header = document.getElementById('header');
 let body = document.querySelector('body');
+let heroSection = document.querySelector('.hero-section');
 
-document.addEventListener('scroll', function () {
-  let scrollPos = window.scrollY;
+header.classList.add('bg-yellow');
 
-  if (header.scrollTop > 350 || body.scrollTop > 350 ) {
-    header.style.backgroundColor = '#FFF';
-    header.classList.remove('bg-yellow');
+window.addEventListener('scroll', function cambioColoreBackground() {
+  let scrollPos = window.scrollY || document.documentElement.scrollTop;
+
+  // Altezza della hero section
+  let heroSectionHeight = heroSection.clientHeight;
+
+  // Condizione per cambiare colore
+  if (scrollPos < heroSectionHeight) {
+    header.classList.add('bg-yellow'); // Colore iniziale
   } else {
-    header.classList.remove('bg-scroll');
-    header.classList.add('bg-yellow');
+    header.classList.remove('bg-yellow');
+    header.style.backgroundColor = '#FFF'; // Colore dopo la hero section
   }
 });
-
-
-
-/* window.onscroll = function cambioColoreBackground () {
-    if (document.body.scrollTop > 350 || document.documentElement.scrollTop > 350) {
-        document.getElementById('buttonHeader').classList.add('green');
-        document.getElementById('header').classList.add('active');
-    } else  {
-        document.getElementById('buttonHeader').classList.remove('green');
-        document.getElementById('header').classList.remove('active');
-    }
-} */
