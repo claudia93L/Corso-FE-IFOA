29622061318b3006f3d8dc15c1a8069cdc85b851
claudia93L <34545518+claudia93L@users.noreply.github.com>
2023-12-01 08:51:08 +0100

    header scroll btn

--- Sett04/Esercizio settimanale/assets/js/script.js
+++ Sett04/Esercizio settimanale/assets/js/script.js
@@ -2,6 +2,8 @@ let header = document.getElementById('header');
 let body = document.querySelector('body');
 let heroSection = document.querySelector('.hero-section');
 
+let buttonHeader = document.querySelector('.navbar .button-cta');
+
 header.classList.add('bg-yellow');
 
 window.addEventListener('scroll', function cambioColoreBackground() {
@@ -13,8 +15,11 @@ window.addEventListener('scroll', function cambioColoreBackground() {
   // Condizione per cambiare colore
   if (scrollPos < heroSectionHeight) {
     header.classList.add('bg-yellow'); // Colore iniziale
+    buttonHeader.classList.remove('button-header-scroll');
   } else {
     header.classList.remove('bg-yellow');
     header.style.backgroundColor = '#FFF'; // Colore dopo la hero section
+    
+    buttonHeader.classList.add('button-header-scroll');
   }
 });
