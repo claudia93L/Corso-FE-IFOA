1904add7de9b82c0788dbfc574ac475c003b579a
claudia93L <34545518+claudia93L@users.noreply.github.com>
2023-12-05 12:29:26 +0100

    esercizio 1 svolto

---
+++ Sett06/G01/01 - Esercizio/assets/js/script.js
@@ -0,0 +1,31 @@
+// ESERCIZIO 1
+
+class User {
+  constructor(firstName, lastName, age, location) {
+    this.firstName = firstName;
+    this.lastName = lastName;
+    this.age = age;
+    this.location = location;
+  }
+
+  getOlderUser(age2) {
+    if (this.age > age2) {
+      console.log(
+        `L'utente ${this.firstName} è più vecchio di ${user2.firstName}`
+      );
+    } else if (this.age === age2) {
+      console.log(`Entrambi gli utenti hanno ${this.age} anni`);
+    } else {
+      console.log(
+        `L'utente ${user2.firstName} è più vecchio di ${this.firstName}`
+      );
+    }
+  }
+}
+
+const user1 = new User('Mario', 'Rossi', 40, 'Roma');
+const user2 = new User('Bruno', 'Verdi', 30, 'Milano');
+
+user1.getOlderUser(user2.age);
+
+// ESERCIZIO 2
