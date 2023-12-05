5c00816639c41e6e94183fff903d8abcf0c378bf
claudia93L <34545518+claudia93L@users.noreply.github.com>
2023-12-05 16:34:44 +0100

    esercizio 2

--- Sett06/G01/01 - Esercizio/assets/js/script.js
+++ Sett06/G01/01 - Esercizio/assets/js/script.js
@@ -31,7 +31,7 @@ user1.getOlderUser(user2.age);
 // ESERCIZIO 2
 
 // base dati per inserire le options corrette nella select
-const pets = [
+const speciesAndBreeds = [
   {
     species: 'dog',
     breeds: ['Golden Retriever', 'Rottweiler', 'Samoiedo'],
@@ -47,9 +47,130 @@ const pets = [
   },
 ];
 
-const petName = document.getElementById('petName');
-const ownerName = document.getElementById('ownerName');
-const selectedSpecies = document.getElementById('selectedSpecies');
-const selectedBreed = document.getElementById('selectedBreed');
+class Pet {
+  constructor(petName, ownerName, selectedSpecies, selectedBreed) {
+    this.petName = petName;
+    this.ownerName = ownerName;
+    this.selectedSpecies = selectedSpecies;
+    this.selectedBreed = selectedBreed;
+  }
 
+  checkMatchingOwner(anotherPet) {
+    return this.ownerName === anotherPet.ownerName;
+  }
+}
 
+const petNameInput = document.getElementById('petName');
+const ownerNameInput = document.getElementById('ownerName');
+let selectedSpeciesSelect = document.getElementById('selectedSpecies');
+let selectedBreedSelect = document.getElementById('selectedBreed');
+const selectedBreedLabel = document.getElementById('selectedBreedLabel');
+const sendButton = document.getElementById('sendButton');
+const divSelect = document.getElementById('divSelect');
+const ul = document.getElementById('ul');
+let pets = [];
+
+// al cambio di opzione sulla select della specie, devo riempire la select della razza con quella attinente alla specie selezionata
+// al click del bottone devo: far scomparire il div del form e mostrare quello della lista
+
+sendButton.onclick = () => {
+  let petName = petNameInput.value;
+  let ownerName = ownerNameInput.value;
+  let selectedSpecies = getSpeciesValue();
+  let selectedBreed = selectedBreedSelect.value;
+
+  console.log(petName);
+  console.log(ownerName);
+  console.log(selectedSpecies);
+  console.log(selectedBreed);
+
+  clearInputs();
+  createPet(petName, ownerName, selectedSpecies, selectedBreed);
+};
+
+// la select ha un problema da correggere
+
+selectedSpeciesSelect.onchange = () => {
+  selectedBreedSelect.innerHTML = '';
+  let selectedSpecies = getSpeciesValue();
+
+  speciesAndBreeds.forEach((el) => {
+    if (el.species === selectedSpecies) {
+      toggleDiv(divSelect);
+      selectedBreedLabel.innerText = `Choose your pet's breed ${selectedSpecies}`;
+
+      let option = document.createElement('option');
+      option.innerText = "Choose your pet's breed";
+      selectedBreedSelect.appendChild(option);
+
+      el.breeds.forEach((breed) => {
+        let breedOption = document.createElement('option');
+        breedOption.innerText = breed;
+        selectedBreedSelect.appendChild(breedOption);
+      });
+    }
+  });
+};
+
+const getSpeciesValue = () => {
+  switch (selectedSpeciesSelect.value) {
+    case '1':
+      return 'dog';
+      break;
+    case '2':
+      return 'cat';
+      break;
+    case '3':
+      return 'rabbit';
+      break;
+    default:
+      return 'valore non valido';
+      break;
+  }
+};
+
+const toggleDiv = (div) => {
+  div.classList.toggle('d-none');
+};
+
+const clearInputs = () => {
+  petNameInput.value = '';
+  ownerNameInput.value = '';
+  selectedSpeciesSelect.value = '0';
+  selectedBreedSelect.value = '';
+};
+
+/* const writeList = (petName, ownerName, selectedSpecies, selectedBreed) => {
+  const formData = [
+    `Nome dell'animale: ${petName}`,
+    `Nome del proprietario: ${ownerName}`,
+    `Specie dell'animale: ${selectedSpecies}`,
+    `Razza dell'animale: ${selectedBreed}`,
+  ];
+
+  formData.forEach((el) => {
+    let li = document.createElement('li');
+    li.innerText = el;
+    ul.appendChild(li);
+  });
+
+  toggleDiv(divForm);
+  toggleDiv(divList);
+}; */
+
+const createPet = (petName, ownerName, selectedSpecies, selectedBreed) => {
+  let newPet = new Pet(petName, ownerName, selectedSpecies, selectedBreed);
+  pets.push(newPet);
+  updatePetList();
+};
+
+const updatePetList = () => {
+  ul.innerHTML = '';
+  pets.forEach((pet) => {
+    let li = document.createElement('li');
+    li.innerText = `Nome dell'animale: ${pet.petName}, Nome del proprietario: ${pet.ownerName}, Specie: ${pet.selectedSpecies}, Razza: ${pet.selectedBreed}`;
+    ul.appendChild(li);
+  });
+};
+
+console.log(checkMatchingOwner());
