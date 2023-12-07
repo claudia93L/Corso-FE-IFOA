70e0701d1f4ff545ad9aa40c97d22d63e4949672
claudia93L <34545518+claudia93L@users.noreply.github.com>
2023-12-07 16:19:11 +0100

    aggiunti bottoni alla card

--- Sett06/G03/01 - Esercizio/assets/js/script.js
+++ Sett06/G03/01 - Esercizio/assets/js/script.js
@@ -15,7 +15,7 @@ function sendDataToHomepage(data) {
 
 function createColumn() {
   const column = document.createElement('div');
-  column.classList.add('col', 'col-md-3', 'col-sm-6', 'col-xs-12', 'col-lg-4');
+  column.classList.add('col-md-4', 'col-sm-6', 'col-lg-3', 'col-xl-2');
 
   return column;
 }
@@ -23,10 +23,41 @@ function createColumn() {
 function createCard(el) {
   const card = document.createElement('div');
   card.classList.add('card');
+
   const img = document.createElement('img');
   img.classList.add('card-img-top');
   img.src = el.img;
   card.appendChild(img);
+
+  const cardBody = document.createElement('div');
+  cardBody.classList.add('card-body');
+  card.appendChild(cardBody);
+
+  const h4 = document.createElement('h4');
+  h4.classList.add('card-title');
+  cardBody.appendChild(h4);
+  h4.textContent = `Titolo: ${el.title}`;
+
+  const h5 = document.createElement('h5');
+  h5.classList.add('card-text');
+  cardBody.appendChild(h5);
+  h5.textContent = `Prezzo: ${el.price}`;
+
+  const cardFooter = document.createElement('div');
+  cardFooter.classList.add('card-footer', 'bg-dark');
+  card.appendChild(cardFooter);
+
+  const btnBuy = document.createElement('button');
+  btnBuy.type = 'button';
+  btnBuy.classList.add('btn', 'btn-primary');
+  btnBuy.textContent = 'Compra ora';
+  cardFooter.appendChild(btnBuy);
+
+  const btnRemove = document.createElement('button');
+  btnRemove.type = 'button';
+  btnRemove.classList.add('btn', 'btn-danger');
+  btnRemove.textContent = 'Scarta';
+  cardFooter.appendChild(btnRemove);
 
   return card;
 }
