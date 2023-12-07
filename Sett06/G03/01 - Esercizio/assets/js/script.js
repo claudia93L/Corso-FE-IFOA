e0f4992398e954ae2d5044a51a6a0288dec0746e
claudia93L <34545518+claudia93L@users.noreply.github.com>
2023-12-07 15:46:58 +0100

    inizio card su js

--- Sett06/G03/01 - Esercizio/assets/js/script.js
+++ Sett06/G03/01 - Esercizio/assets/js/script.js
@@ -1,1 +1,32 @@
 const url = 'https://striveschool-api.herokuapp.com/books';
+const row = document.getElementById('row');
+
+fetch(url)
+  .then((response) => response.json())
+  .then((data) => sendDataToHomepage(data));
+
+function sendDataToHomepage(data) {
+  data.forEach((el) => {
+    const column = createColumn();
+    row.appendChild(column);
+    column.appendChild(createCard(el));
+  });
+}
+
+function createColumn() {
+  const column = document.createElement('div');
+  column.classList.add('col', 'col-md-3', 'col-sm-6', 'col-xs-12', 'col-lg-4');
+
+  return column;
+}
+
+function createCard(el) {
+  const card = document.createElement('div');
+  card.classList.add('card');
+  const img = document.createElement('img');
+  img.classList.add('card-img-top');
+  img.src = el.img;
+  card.appendChild(img);
+
+  return card;
+}
