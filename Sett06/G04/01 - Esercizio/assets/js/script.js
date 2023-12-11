d39152b0c2c3b937adaba784e591dd2ce64a654c
claudia93L <34545518+claudia93L@users.noreply.github.com>
2023-12-11 18:08:20 +0100

    fix dell'extra della modale da fare

--- Sett06/G04/01 - Esercizio/assets/js/script.js
+++ Sett06/G04/01 - Esercizio/assets/js/script.js
@@ -43,12 +43,12 @@ function loadPhotos(photos) {
     const card = document.createElement('div');
     card.classList.add('col-4');
     card.innerHTML = `<div class="card mb-4 shadow-sm">
-      <img class="card-img-top" src="${el.src.medium}" alt="${el.alt}" />  
+    <img class="card-img-top" src="${el.src.medium}" alt="${el.alt}" data-bs-toggle="modal" data-bs-target="#modal" />
       <div class="card-body">
-        <h5 class="card-title">${el.alt}</h5>
+        <h5 class="card-title" data-bs-toggle="modal" data-bs-target="#modal">${el.alt}</h5>
         <div class="d-flex justify-content-between align-items-center">
           <div class="btn-group">
-            <button type="button" class="btn btn-sm btn-outline-secondary">
+            <button type="button" class="btn btn-sm btn-outline-secondary viewImgBtn">
               View
             </button>
             <button type="button" class="btn btn-sm btn-outline-secondary hideBtn">
@@ -61,10 +61,26 @@ function loadPhotos(photos) {
     </div>`;
     photoRow.appendChild(card);
 
+    const img = card.querySelector('img');
+    const h5 = card.querySelector('.card-title');
+
     const hideBtn = card.querySelector('.hideBtn');
+    const viewImgBtn = card.querySelector('.viewImgBtn');
+
+    img.onclick = () => {
+      createModalDetails(el);
+    };
+
+    h5.onclick = () => {
+      createModalDetails(el);
+    };
 
     hideBtn.onclick = () => {
       hideCard(card);
+    };
+
+    viewImgBtn.onclick = () => {
+      createModalPhoto(el);
     };
   });
 }
@@ -79,6 +95,22 @@ searchBtn.onclick = () => {
   getPhotos(searchedQuery);
   loadPhotos(photos);
 };
+
+function createModalDetails(el) {
+  const modalContent = document.getElementById('modal-content');
+  modalContent.innerHTML = `
+  <img class="card-img-top" src="${el.src.medium}" alt="${el.alt}" />
+  <h5 class="card-title">${el.photographer}</h5>
+  <a href="${el.photographer_url}" target="_blank">${el.photographer_url}</a>
+`;
+}
+
+// sistemare apertura della seconda modale
+
+function createModalPhoto(el) {
+  const modalContent = document.getElementById('modal-content');
+  modalContent.innerHTML = `<img class="card-img-top" src="${el.src.landscape}" alt="${el.alt}" />`;
+}
 
 window.onload = () => {
   getPhotos();
