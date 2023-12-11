57400a1597defaf5c08d2f24d9b3e5f16f736b9a
claudia93L <34545518+claudia93L@users.noreply.github.com>
2023-12-11 14:41:18 +0100

    settaggi iniziali

--- Sett06/G04/01 - Esercizio/assets/js/script.js
+++ Sett06/G04/01 - Esercizio/assets/js/script.js
@@ -7,6 +7,13 @@ const header = {
 };
 const photos = [];
 
+const loadImagesBtn = document.getElementById('loadImagesBtn');
+const loadSecondaryImagesBtn = document.getElementById(
+  'loadSecondaryImagesBtn'
+);
+
+const photoRow = document.getElementById('photo-row');
+
 const getPhotos = () => {
   fetch(url, {
     headers: header,
@@ -17,3 +24,55 @@ const getPhotos = () => {
       loadPhotos(data);
     });
 };
+
+function loadPhotos(data) {
+    data.photos.forEach(el => {
+        photoRow.innerHTML = `<div class="col-md-4">
+        <div class="card mb-4 shadow-sm">
+          <svg
+            class="bd-placeholder-img card-img-top"
+            width="100%"
+            height="225"
+            xmlns="http://www.w3.org/2000/svg"
+            preserveAspectRatio="xMidYMid slice"
+            focusable="false"
+            role="img"
+            aria-label="Placeholder: Thumbnail"
+          >
+            <title>${}</title>
+            <rect width="100%" height="100%" fill="#55595c" />
+            <text x="50%" y="50%" fill="#eceeef" dy=".3em">
+              Thumbnail
+            </text>
+          </svg>
+          <div class="card-body">
+            <h5 class="card-title">Lorem Ipsum</h5>
+            <p class="card-text">
+              This is a wider card with supporting text below as a natural
+              lead-in to additional content. This content is a little bit
+              longer.
+            </p>
+            <div
+              class="d-flex justify-content-between align-items-center"
+            >
+              <div class="btn-group">
+                <button
+                  type="button"
+                  class="btn btn-sm btn-outline-secondary"
+                >
+                  View
+                </button>
+                <button
+                  type="button"
+                  class="btn btn-sm btn-outline-secondary"
+                >
+                  Edit
+                </button>
+              </div>
+              <small class="text-muted">9 mins</small>
+            </div>
+          </div>
+        </div>
+      </div>`
+    })
+}
