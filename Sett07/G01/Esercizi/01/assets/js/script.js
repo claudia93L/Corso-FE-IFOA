e6f7757fec0a41abcb84d565393456470baf08d8
claudia93L <34545518+claudia93L@users.noreply.github.com>
2023-12-20 12:33:00 +0100

    migliorie seguendo la correzione

--- Sett07/G01/Esercizi/01/assets/js/script.js
+++ Sett07/G01/Esercizi/01/assets/js/script.js
@@ -1,37 +1,39 @@
-const arrayNumbers = [36, 88, 20, 55, 4, 2, 75, 48];
-
 class classNumbers {
-  constructor(arrayNumbers) {
-    this.arrayNumbers = arrayNumbers;
+  constructor(array = null) {
+    if (array !== null) {
+      this.numbers = array;
+    }
   }
 
-  checkProperty() {
-    if (classOne.hasOwnProperty(arrayNumbers)) {
+  /*  checkProperty() {
+    if (classOne.hasOwnProperty(numbers)) {
       console.log('ha la proprietà');
     } else {
       console.log('non ha la proprietà');
     }
   }
-
+ */
   checkNumbers() {
-    arrayNumbers.forEach((number) => {
-      if (number % 3 === 0) {
-        console.log('pippo');
+    this.numbers.forEach((number) => {
+      if (number % 3 === 0 && number % 5 === 0) {
+        console.log('paperino');
       } else if (number % 5 === 0) {
         console.log('pluto');
+      } else if (number % 3 === 0) {
+        console.log('pippo');
       } else if (number % 3 === 0 && number % 5 === 0) {
         console.log('paperino');
       } else {
-        console.log(`${number}`);
+        console.log(number.toString());
       }
     });
   }
-  
 }
+
+const arrayNumbers = [36, 88, 20, 55, 4, 2, 75, 48, 15];
 
 let classOne = new classNumbers(arrayNumbers);
 
-classOne.checkProperty();
+//classOne.checkProperty();
 
 classOne.checkNumbers();
-
