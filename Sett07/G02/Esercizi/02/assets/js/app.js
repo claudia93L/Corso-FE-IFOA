35ae5a1cc86b7d012066b41ea52e35da140e3056
claudia93L <34545518+claudia93L@users.noreply.github.com>
2023-12-21 16:25:20 +0100

    hide/show ball

--- Sett07/G02/Esercizi/02/assets/js/app.js
+++ Sett07/G02/Esercizi/02/assets/js/app.js
@@ -1,3 +1,5 @@
+const ball = document.getElementById('palla');
+
 function addPoints() {
   const score = document.getElementById('number');
   let points = 0;
@@ -14,10 +16,10 @@ function startGame() {
   const welcome = document.getElementById('welcome');
 
   welcome.style.display = 'none';
+  ball.classList.remove('d-none');
+  moveBallX();
 }
 
 function moveBallX() {
   const ball = document.getElementById('palla');
-
-  
 }
