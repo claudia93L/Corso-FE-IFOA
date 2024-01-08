59186885b6eeb040189254e554075db32c479700
claudia93L <34545518+claudia93L@users.noreply.github.com>
2024-01-08 18:55:04 +0100

    edit tag img

--- Sett08/G01/01Esercizio/reactApp/src/components/ImageFromParent.jsx
+++ Sett08/G01/01Esercizio/reactApp/src/components/ImageFromParent.jsx
@@ -2,9 +2,7 @@ import React, { Component } from 'react';
 
 class ImageFromParent extends Component {
   render() {
-    return (
-      <img className='monkey' src={this.props.src} alt={this.props.alt}></img>
-    );
+    return <img className='monkey' src={this.props.src} alt={this.props.alt} />;
   }
 }
 
