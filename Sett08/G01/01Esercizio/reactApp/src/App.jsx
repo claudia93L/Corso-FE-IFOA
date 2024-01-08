57098e25e418ac46f14afb091dbd7017f513f847
claudia93L <34545518+claudia93L@users.noreply.github.com>
2024-01-08 17:34:50 +0100

    cancellati i default di react

--- Sett08/G01/01Esercizio/reactApp/src/App.jsx
+++ Sett08/G01/01Esercizio/reactApp/src/App.jsx
@@ -19,34 +19,14 @@ function App() {
 
   return (
     <>
-      <div>
-        <a href='https://vitejs.dev' target='_blank'>
-          <img src={viteLogo} className='logo' alt='Vite logo' />
-        </a>
-        <a href='https://react.dev' target='_blank'>
-          <img src={reactLogo} className='logo react' alt='React logo' />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
+      <h1>Prime prove React props</h1>
       <div className='card'>
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
         <ButtonComponent></ButtonComponent>
         <ButtonSecondary textContent='Soluzione alternativa'></ButtonSecondary>
         <ButtonFromParent testo={btnText}></ButtonFromParent>
         <ImageComponent></ImageComponent>
-        <ImageFromParent
-          src={imageSrc}
-          alt={imageAlt}
-        ></ImageFromParent>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
+        <ImageFromParent src={imageSrc} alt={imageAlt}></ImageFromParent>
       </div>
-      <p className='read-the-docs'>
-        Click on the Vite and React logos to learn more
-      </p>
     </>
   );
 }
