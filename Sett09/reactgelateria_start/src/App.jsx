3f339f2735827be7bf1e59d271d752b7028816f7
claudia93L <34545518+claudia93L@users.noreply.github.com>
2024-01-15 15:17:08 +0100

    start files esercizi react

---
+++ Sett09/reactgelateria_start/src/App.jsx
@@ -0,0 +1,15 @@
+import { useState } from 'react'
+
+// endpoint gelati     const url = "https://react--course-api.herokuapp.com/api/v1/data/gelateria";
+
+function App() {
+  const [count, setCount] = useState(0)
+
+  return (
+    <>
+      
+    </>
+  )
+}
+
+export default App
