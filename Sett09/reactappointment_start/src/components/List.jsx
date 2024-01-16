5c2ff7ea671dafb258fe178f55fe81df06b93abc
claudia93L <34545518+claudia93L@users.noreply.github.com>
2024-01-16 14:16:36 +0100

    inizio componenti a classe

---
+++ Sett09/reactappointment_start/src/components/List.jsx
@@ -0,0 +1,9 @@
+import Component from 'react';
+
+class List extends React.Component {
+  render() {
+    return <></>;
+  }
+}
+
+export default List;
---
+++ Sett09/reactappointment_start/src/components/SingleAppointment.jsx
@@ -0,0 +1,9 @@
+import Component from 'react';
+
+class SingleAppointment extends React.Component {
+  render() {
+    return <></>;
+  }
+}
+
+export default SingleAppointment;
